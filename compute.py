3ba7cd4f25d10a81206288c7f2ede643381ac55f
godobyte <godot.y.bian@gmail.com>
2018-07-30 12:57:30 -0700

    Added best response basics, debugging

--- compute.py
+++ compute.py
@@ -21,6 +21,10 @@ alpha2 = 0
 lambda_za = 0.5
 lambda_zb = 0.5
 
+#################################################
+# Profit Evaluation for Airline 1 and Airline 2 #
+#################################################
+
 def profit_eva_1(p1_za, p1_zb, p2_za, p2_zb):
     pi1_za = 0
     if p1_za <= p2_za + delta_za:
@@ -55,6 +59,57 @@ def profit_eva_2(p1_za, p1_zb, p2_za, p2_zb):
 
     return lambda_za * pi2_za + lambda_zb * pi2_zb
 
+###########################
+# Airlines' Best Response #
+###########################
+class Response:
+    def __init__(self, name, pi):
+        self.name = name
+        self.pi = pi
+
+    def update(self, new_name, new_pi):
+        self.name = new_name
+        self.pi = new_pi
+
+# Set up parameters
+interval = 0.01
+epsilon = 0.01
+
+# Fix pi_za and p1_zb to find p2_za and p2_zb to maximize pi2
+def best_response_2(p1_za, p1_zb):
+    p2_za = 0
+    p2_zb = 0
+    print("break a")
+
+    #Candidate 1
+    if (p1_zb + delta_zb >= p1_za - delta_za) or (p1_za - delta_za <= c2 + alpha2):
+        p2_za = min(p1_zb + delta_zb, 1)
+        p2_zb = p2_za
+        res = Response("candidate 1", profit_eva_2(p1_za, p1_zb, p2_za, p2_zb))
+    print("break b")
+
+    #Candidate 2
+    if p1_za - delta_za > c2 + alpha2:
+        p2_zb = min(p1_zb + delta_zb, 1)
+        p2_za = p1_za - delta_za - epsilon
+        pi_2 = profit_eva_2(p1_za, p1_zb, p2_za, p2_zb)
+        if pi_2 > res.pi:
+            res = update("candidate 2", pi_2)
+    print("break c")
+
+    #Candidate 3
+    if (p1_za > p1_zb) and (p1_zb - delta_za > c2 + alpha2):
+        p2_zb = min(p1_zb + delta_zb, 1)
+        p2_za = p1_zb - delta_za - epsilon
+        pi_2 = profit_eva_2(p1_za, p1_zb, p2_za, p2_zb)
+        if pi_2 > res.pi:
+            res = update("candidate 3", pi_2)
+    print("break d")
+
+    print("When p1_za = %s, p1_zb = %s, p2_za = %s, p2_zb = %s, the best response is %s with pi2 = %s"
+    % (p1_za, pi1_zb, p2_za, p2_zb, res.name, res.pi))
+    return 0
+
 if __name__ == "__main__":
     import sys
     print(__doc__)
@@ -63,6 +118,8 @@ if __name__ == "__main__":
         profit_eva_1(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])))
         print("Executing profit evalutation for Airline 2: %s" %
         profit_eva_2(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4])))
+        print("Executing best Candidate for Airline 2 pricing \n")
+        best_response_2(int(sys.argv[1])), int(sys.argv[2])
         print("Execution done")
     except (IndexError, TypeError):
         pass
